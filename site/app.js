db73a021aad767ab09f45a12275d4dcbab2fd260
JuaMateu <juanma.mateu@gmail.com>
2022-05-08 11:21:42 -0300

    agregue los html que faltaban (login productDetail shop, etc) y quedaron todos los links ruteados

--- site/app.js
+++ site/app.js
@@ -11,8 +11,21 @@ const public = path.join(__dirname, 'public/')
 // http routes
 const httpRaiz = '/'
 const httpHome = '/home'
+const httpLogin = '/login'
+const httpCart = '/productCart'
+const httpProductDetail = '/productDetail'
+const httpRegister = '/Register'
+const httpShop = '/shop'
+const httpUserRecovery = '/userRecovery'
 // html
 const homeHtml = 'home.html'
+const LoginHtml = 'login.html'
+const productCartHtml = 'productCart.html'
+const productDetailHtml = 'productDetail.html'
+const RegisterHtml = 'Register.html'
+const shopHtml = 'shop.html'
+const userRecoveryHtml = 'userRecovery.html'
+
 
 // Define the static file path
 app.use(express.static(__dirname +'/public/'));
@@ -21,9 +34,26 @@ app.use(express.static(__dirname +'/public/'));
 app.get(httpRaiz, (req, res) => {
     res.sendFile(path.join(views, homeHtml))
 })
-
 app.get(httpHome, (req, res) => {
     res.sendFile(path.join(views, homeHtml))
+})
+app.get(httpLogin, (req, res) => {
+    res.sendFile(path.join(views, LoginHtml))
+})
+app.get(httpCart, (req, res) => {
+    res.sendFile(path.join(views, productCartHtml))
+})
+app.get(httpProductDetail, (req, res) => {
+    res.sendFile(path.join(views, productDetailHtml))
+})
+app.get(httpRegister, (req, res) => {
+    res.sendFile(path.join(views, RegisterHtml))
+})
+app.get(httpShop, (req, res) => {
+    res.sendFile(path.join(views, shopHtml))
+})
+app.get(httpUserRecovery, (req, res) => {
+    res.sendFile(path.join(views, userRecoveryHtml))
 })
 
 app.listen(port, () => {
