06546815488d87f34e61a69b47bf85460b82c20e
ioai2610 <iorlandoayala@hotmail.com>
2022-06-05 13:24:02 -0500

    users DB,edit,add & list  archives created and minimal functions

--- site/src/controllers/usersController.js
+++ site/src/controllers/usersController.js
@@ -1,3 +1,9 @@
+const fs = require('fs');
+const path = require('path');
+
+const usersFilePath = path.join(__dirname, '../data/usersDB.json');
+var users = JSON.parse(fs.readFileSync(usersFilePath, 'utf-8'));
+
 const controller = {
     login: (req, res) => {
         return res.render("users/login");
@@ -8,7 +14,12 @@ const controller = {
     userRecovery: (req, res) => {
         return res.render("users/userRecovery");
     },
-    
+    list: (req,res) => {
+        return res.render("users/usersList", {users});
+    },
+    addForm : (req,res) => {
+        return res.render("users/usersAdd");
+    }
 }
 
 module.exports = controller
--- site/src/routes/usersRoutes.js
+++ site/src/routes/usersRoutes.js
@@ -7,4 +7,10 @@ router.get('/login', usersController.login);
 router.get('/register', usersController.register);
 router.get('/userRecovery', usersController.userRecovery);
 
+//* CRUD de Users *//
+// listado de los usuarios
+router.get('/',usersController.list);
+
+router.get('/create',usersController.addForm);
+
 module.exports = router;
