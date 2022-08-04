599074886d7678a30fca6faddd896d9ff363b846
ioai2610 <iorlandoayala@hotmail.com>
2022-08-04 18:28:07 -0500

    COMMIT DE BRUNO: Edit listo

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -97,38 +97,79 @@ const controller = {
             });
     },
     
-    editForm:(req,res)=> {
+    editForm: (req, res) => {
         // READ
         // recibimos el id del producto a editar
         // filtramos el producto con el id indicado
         // usamos destructuring para obtener el objeto relativo al producto
-        let  [ editProduct ]  = products.filter(prod => {return prod.id == req.params.id});
+        // let [editProduct] = products.filter((prod) => {
+        //  return prod.id == req.params.id;
+        // });
         // renderizamos la vista con el elemento correpondiente
-        res.render('../views/products/editProducts.ejs',{editProduct:editProduct});
-    },
-    edit: (req,res) => {
+        // res.render("../views/products/editProducts.ejs", {
+        //  editProduct: editProduct,
+        //});
+        db.Product.findByPk(req.params.id).then((product) => {
+          return res.render("products/editProducts", { editProduct: product });
+        });
+      },
+      edit: (req, res) => {
+        let id = req.params.id;
+        let productType = 0;
+        let productImage = "/img/plantas/Aglaonema.png";
+        req.body.type == "Planta Floral" ? (productType = 1) : "";
+        req.body.type == "Aromatica" ? (productType = 2) : "";
+        req.body.type == "Arbusto" ? (productType = 3) : "";
+        req.body.type == "Suculenta" ? (productType = 4) : "";
+        req.body.type == "Frutal" ? (productType = 5) : "";
+        if (req.file) {
+          productImage = "/img/plantas/" + req.file.filename;
+        }
+        db.Product.update(
+          {
+            name: req.body.name,
+            type: productType,
+            careLevel: req.body.careLevel,
+            description: req.body.description,
+            stock: req.body.stock,
+            price: req.body.price,
+            label: req.body.label,
+            img: productImage,
+          },
+          {
+            where: {
+              id: id,
+            },
+          }
+        ).then((product) => {
+          return res.redirect("/products/edit/" + id);
+        });
+        /*
         // UPDATE
         // Obtenemos el producto a editar segun el id recibido
-        let  [ editProduct ]  = products.filter(prod => {return prod.id == req.params.id});
+        let [editProduct] = products.filter((prod) => {
+          return prod.id == req.params.id;
+        });
         // editamos el producto con los datos entrantes
-        editProduct.name = req.body.name
-        editProduct.type = req.body.type
-        editProduct.careLevel = req.body.careLevel
-        editProduct.description = req.body.description
-        editProduct.stock = req.body.stock
-        editProduct.price = req.body.price
-        editProduct.label = req.body.label
+        editProduct.name = req.body.name;
+        editProduct.type = req.body.type;
+        editProduct.careLevel = req.body.careLevel;
+        editProduct.description = req.body.description;
+        editProduct.stock = req.body.stock;
+        editProduct.price = req.body.price;
+        editProduct.label = req.body.label;
         //validar si cargaron imagen y cambiarla, si no no hacer nada
         if (req.file) {
-            editProduct.img = "/img/plantas/" + req.file.filename
+          editProduct.img = "/img/plantas/" + req.file.filename;
         }
-        editProduct.sells = 0
+        editProduct.sells = 0;
         // sobreescribimos el JSON
         let productsExport = JSON.stringify(products, null, 2);
-        fs.writeFileSync(productsFilePath, productsExport,'utf-8');
+        fs.writeFileSync(productsFilePath, productsExport, "utf-8");
         // redirigimos a la lista de productos
-        res.redirect("/products/edit/"+ req.params.id);
-    },
+        res.redirect("/products/edit/" + req.params.id);
+        */
+      },
     delete:(req,res)=> {
         // DELETE
         // solcitamos el id y con filter, pedimos devolver todos los elementos cuyo id sea distinto al entrante
