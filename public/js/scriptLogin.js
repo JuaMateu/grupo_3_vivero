33c100002c6724fa0158aefe1eea6520c8dd39ae
ioai2610 <iorlandoayala@hotmail.com>
2022-08-11 23:16:20 -0500

    Adicion de funciones para validar login desde front

--- public/js/scriptLogin.js
+++ public/js/scriptLogin.js
@@ -13,35 +13,62 @@ window.addEventListener('load',function(){
         paragraph.classList.toggle("text-danger");
         paragraph.innerText = message;
         element.appendChild(paragraph);
-        // paragraph.style.display = "none";
         errores.push(1);
-    }
+    };
 
     // Ocultamos los iconos de "check" al cargar la pagina
-    for (i=0; i<OkIcon.length; i++){
-        OkIcon[i].style.display = "none";
+    hideOk = () => {
+        for (i=0; i<OkIcon.length; i++){
+            OkIcon[i].style.display = "none";
+        }
+    };
+
+    //Mostrar mensajes de error
+    showOk = () => {
+        for (i=0; i<OkIcon.length; i++){
+            OkIcon[i].style.display = "block";
+        }
+    };
+
+    //Eliminacion de mensaje de error y puesta de vuelta a iconos
+    continousMessage = () => {
+        hideOk();
+            setTimeout(function(){
+                let hideError = document.querySelector('p'); // seleccionamos elemnto creado en linea 12
+                hideError.style.display = "none"
+                showOk();
+            },5000);
     }
 
+    // Frenamos el form para validar
     formLogin.addEventListener("submit",function(event){
         // Chequeamos si email vacio
         if (!email.value) {
-            setTimeout(showMessage(divEmail,"El campo de email debe estar completo"),5000); // probando temporizador
+            showMessage(divEmail,"El campo de email debe estar completo");
+            continousMessage();
         } else if (!email.value.includes("@") || !email.value.includes(".com")) {
             showMessage(divEmail,"El campo de email debe ser válido");
+            continousMessage();
         }
         // Chequeamos si password vacio
         if (!password.value) {
             showMessage(divPassword,"El campo de password debe estar completo");
-
+            continousMessage(); // no llega el scoope de la funcion REVISAR
         } else if (password.value.length < 8) {
             showMessage(divPassword,"La contraseña debe tener mínimo 8 caracteres");
-        }
+            continousMessage(); // no llega el scoope de la funcion REVISAR
+        };
         // Si hay errores, detenemos el envio del form
         console.log("veamos errores");
         if (errores.length > 1) {
-            event.preventDefault();
             console.log(errores.length);
+            event.preventDefault();
         }
+
+        // Si los errores superan a 2 se recarga para evitar aglomeracion de mensajes de error
+        if (errores.length > 2){
+            location.reload(); 
+        };
     });
 });
 
