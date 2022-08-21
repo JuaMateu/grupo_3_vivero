fa5f5aa6f32bedfbedeff623b8d6dacede476689
Bruno Secchiari <brunosecchiari95@gmail.com>
2022-08-20 21:59:17 -0300

    Trabajando en el CSS del carrito de compras

--- public/js/shop.js
+++ public/js/shop.js
@@ -1,18 +1,19 @@
 window.addEventListener("load", () => {
   // localStorage.clear();
 
-  let buttons = document.querySelectorAll(".btn--plant");
+  const body = document.querySelector("body");
+  const buttons = document.querySelectorAll(".btn--plant");
 
   buttons.forEach((button) => {
     button.addEventListener("click", (event) => {
       event.preventDefault();
 
-      let previousSibling = button.previousElementSibling;
-      let children = previousSibling.children;
-      let name = children[1].innerText;
-      let price = children[2].innerText;
-      let package = children[0];
-      let image = package.children[0].src;
+      const previousSibling = button.previousElementSibling;
+      const children = previousSibling.children;
+      const name = children[1].innerText;
+      const price = children[2].innerText;
+      const package = children[0];
+      const image = package.children[0].src;
 
       if (localStorage.getItem("products")) {
         let products = JSON.parse(localStorage.getItem("products"));
@@ -24,11 +25,27 @@ window.addEventListener("load", () => {
             { name: name, price: price, image: image },
           ])
         );
+
+        const messageBox = document.createElement("div");
+        messageBox.innerText = `${name} se ha agregado al carrito exitosamente!`;
+        messageBox.classList.add("cart__message-box");
+        body.appendChild(messageBox);
+        setTimeout(() => {
+          messageBox.remove();
+        }, 5000);
       } else {
         localStorage.setItem(
           "products",
           JSON.stringify([{ name: name, price: price, image: image }])
         );
+
+        const messageBox = document.createElement("div");
+        messageBox.innerText = `${name} se ha agregado al carrito exitosamente!`;
+        messageBox.classList.add("cart__message-box");
+        body.appendChild(messageBox);
+        setTimeout(() => {
+          messageBox.remove();
+        }, 5000);
       }
     });
   });
