c512866e06972e761f340b087f14ddaa08b62a64
Bruno Secchiari <brunosecchiari95@gmail.com>
2022-09-12 18:55:29 -0300

    Finalizando Vistas & CSS

--- public/js/usersMenuPassword.js
+++ public/js/usersMenuPassword.js
@@ -1,11 +1,24 @@
 window.addEventListener("load", () => {
-  // Left Column
+  /* // Left Column
 
   const list = document.querySelector(".user-menu__information-list");
   list.classList.add("user-menu__information-list-toggle");
 
   const userContact = document.querySelector(".user-menu__contact");
-  userContact.classList.toggle("user-menu__contact-toggle");
+  userContact.classList.toggle("user-menu__contact-toggle"); */
+
+  /* // Right Column
+
+  const loader = document.querySelector(".user-menu__loader");
+  const rightColumn = document.querySelector(".right-column");
+
+  setTimeout(() => {
+    loader.style.opacity = 0;
+    loader.style.display = "none";
+
+    rightColumn.style.display = "block";
+    setTimeout(() => (rightColumn.style.opacity = 1), 1000);
+  }, 2000); */
 
   // Form
 
